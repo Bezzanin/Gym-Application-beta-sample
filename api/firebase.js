33611f1585de3a0b392da928c836fa23d408ffe8
Bezzanin <shoxy95@mail.ru>
2019-09-30 18:17:03 +0300

    Sample Commit

--- api/firebase.js
+++ api/firebase.js
@@ -8,11 +8,11 @@ class Firebase {
      */
     static initialise() {
         firebase.initializeApp({
-            apiKey: "AIzaSyCIoYGPPcwoXacoD8TNm6t9YzgkNcPSh-U",
-            authDomain: "gymnext-a9231.firebaseapp.com",
-            databaseURL: "https://gymnext-a9231.firebaseio.com",
-            storageBucket: "gymnext-a9231.appspot.com",
-            messagingSenderId: "1054828973938"
+            apiKey: "",
+            authDomain: "",
+            databaseURL: "",
+            storageBucket: "",
+            messagingSenderId: ""
         });
     }
 
